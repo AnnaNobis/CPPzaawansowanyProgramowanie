edce324feb690c14e164342752bd85eafd4cb00c
Anna Nobis <anianobis@outlook.com>
2022-09-03 10:52:06 +0200

    20220903 zadanie7

--- Zadanie6/Zadanie6.cpp
+++ Zadanie6/Zadanie6.cpp
@@ -15,37 +15,51 @@ for (int i = 1; i < 1001; i++)
     numbers.push_back(i);
 
 
+std::cout << "Liczby od 1 do 1000: " << std::endl;
 for (auto it = numbers.begin(); it != numbers.end(); ++it)
 {
     std::cout << *it << "; ";
 }
+std::cout << std::endl << std::endl;
 
+//tworzê listê liczb pierwszych
 std::list <int> firstNumbers;
-for (int i = 1; i < 1001; i++)
-    firstNumbers.push_back(i);
 
 std::copy_if(numbers.begin(), numbers.end(), std::back_inserter(firstNumbers),
     [](int x)
-
-    {  if (x < 2)
+    { 
+    if (x < 2)
     return false;
-for (int i = 2; i * i <= x; ++i)
-if (x % i == 0)
-return false;
-return true; });
+    for (int i = 2; i * i <= x; ++i)
+    if (x % i == 0)
+    return false;
+    return true; 
+    });
 
-        
+std::cout<<std::endl << "Liczby pierwsze: " << std::endl;
 for (auto it =firstNumbers.begin(); it != firstNumbers.end(); ++it)
 {
     std::cout << *it << "; ";
 }
-
 std::cout << std::endl << std::endl;
 
+//tworzê listê nie liczb pierwszych
 auto removeFirstNumbers = [firstNumbers](int i) { return firstNumbers.end() != std::find(firstNumbers.begin(), firstNumbers.end(), i); };
 numbers.erase(std::remove_if(numbers.begin(), numbers.end(), removeFirstNumbers), numbers.end());
 std::list<int> notFirstNumbers = numbers;
 
+//druga wersja - zaprzeczenie lambdy z listy liczb pierwszych (chyba prostsza wersja)
+//std::copy_if(numbers.begin(), numbers.end(), std::back_inserter(notFirstNumbers),
+//    [](int x)
+//
+//  {   if (x < 2)
+//      return true;
+//      for (int i = 2; i * i <= x; ++i)
+//      if (x % i == 0)
+//      return true;
+//      return false; });
+
+std::cout << std::endl << "Liczby nie pierwsze to: " << std::endl;
 
 for (auto it = notFirstNumbers.begin(); it != notFirstNumbers.end(); ++it)
 {
--- Zadanie7/Zadanie7.cpp
+++ Zadanie7/Zadanie7.cpp
@@ -6,11 +6,82 @@
 
 #include <iostream>
 #include <list>
+#include <random>
+#include <algorithm>
+
+
+std::list <int> generateList()
+{
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::uniform_int_distribution<int> dist(1, 500);
+
+    std::list<int> listToFill;
+
+    for (unsigned int i = 0; i <= 50; ++i)
+    {
+        listToFill.push_back(dist(generator));
+    }
+
+    return listToFill;
+}
+
+
+void printList (std::list<int> list)
+{
+    for (auto n : list)
+    {
+        std::cout << n << ", ";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+
+
+std::list<int> sortList(std::list<int> list)
+{
+    std::list<int> evenNumbers;
+    std::list<int> oddNumbers;
+    std::list<int> sortedList;
+  
+
+    for (auto it = list.begin(); it != list.end(); ++it)
+    {
+        if ((*it) % 2 == 0)
+        {
+            evenNumbers.push_back(*it);
+        }
+        else
+        {
+            oddNumbers.push_back(*it);
+        }
+    }
+
+    std::sort(evenNumbers.begin(), evenNumbers.end());
+    std::sort(oddNumbers.rbegin(), oddNumbers.rend());
+
+    sortedList = evenNumbers.merge(oddNumbers);
+
+    return sortedList;
+}
+
 
 int main()
 {
-	std::list<int> positiveNumbers;
+    std::cout << "Stworz listê 50 liczb ca³kowitych dodatnich i wype³nij j¹ losowymi wartoœciami.Wypisz j¹." << std::endl;
+
+    std::list<int> listOfNumbers =  generateList();
+    printList(listOfNumbers);
+
+
+    std::cout << "Nastêpnie posortuj i wypisz listê tak, by na pocz¹tku znalaz³y siê wartoœci parzyste uporz¹dkowane rosn¹co, a za nimi wartoœci nieparzyste uporz¹dkowane malej¹co." << std::endl;
+    sortList(listOfNumbers);
+    
+    printList(listOfNumbers);
+   
 
 
 }
 
+
+     
